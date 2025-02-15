c875c601b0e45f00d251645934eeb9148fa55919
Dhanush S <dhanush.utility@gmail.com>
2025-02-15 20:02:42 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+x = document.getElementById("navi");
+
+function sidebar(){
+    if (x.style.display=='flex'){
+        x.style.display='none';
+    } else {
+        x.style.display='flex';
+    }
+}
